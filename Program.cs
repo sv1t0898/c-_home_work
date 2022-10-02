52a2b62e0f9212945464de3fd52a549e7bb377c9
dude <dude@dude.ru>
2022-10-02 20:56:45 +0300

    homework_lesson_5

--- Program.cs
+++ Program.cs
@@ -1,75 +1,70 @@
-﻿//ВСЕ ЗАДАЧИ РЕШАЕМ ЧЕРЕЗ МЕТОДЫ
+﻿// Все массивы от 5-ти элементов
 
-// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
-//возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
-Console.WriteLine("Задача 25");
-Console.WriteLine("Введите число А: ");
-int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число B: ");
-int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Задача 34");
+int[] array1 = GetArray(15, 100, 999);
+Console.WriteLine($"[{String.Join(",", array1)}] -> {ParytiCheck(array1)}");
 
-// Console.WriteLine($"1.{A} в степени - {B} -> {PowConv(A, B)}");
-Console.WriteLine($"2.{A} в степени - {B} -> {PowConvCycle(A, B)}");
-
-// double PowConv(int a, int b){
-//     return (double)Math.Pow(a, b);
-// }
-// Console.WriteLine();
-
-double PowConvCycle(int a, int b){
-    double pow = a;
-    for(int i = 1; i < b; i++){
-        pow = pow * a;
+int ParytiCheck(int[] array){
+    int index = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i] % 2 == 0){
+            index++;
+        }
     }
-    return pow;
-
+    return index;
 }
 Console.WriteLine();
 
 
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
-//сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
-Console.WriteLine("Задача 27");
-Console.WriteLine("Введите число: ");
-int num = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"{num} -> {SumNum(num)}");
+Console.WriteLine("Задача 36");
+int[] array2 = GetArray(7, -11, 99); //new int[4] {-4, -6, 89, 6};
+Console.WriteLine($"[{String.Join(",", array2)}] -> {SumElements(array2)}");
 
-int SumNum(int a){
-    int b = a;
-    int sum = 0;
-    for(int i = 1; b != 0; i++){
-        sum = sum + (b%10);
-        b = b/10;
+int SumElements(int[] array){
+    int conclusion = 0;
+    for (int i = 1; i < array2.Length; i += 2){
+        conclusion += array2[i]; 
     }
-    return sum;
-}   
-Console.WriteLine();
-
-
-// Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов
-// и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]
-
-Console.WriteLine("Задача 29");
-Console.WriteLine($" -> [{String.Join(", ", GenArray())}]");
-
-int[] GenArray(){
-    int[] a = new int[8];
-    for(int i = 0; i < a.Length; i++){
-        //Console.Write($"{i}/{a.Length}.Введите рандомный элемент для добавления в массив:");
-        //int num = int.Parse(Console.ReadLine()!);
-        int num = new Random().Next(0, 100);
-        a[i] = num;
-    }
-    Console.Write($"{String.Join(", ", a)}");
-    return (int[])a;
+    return conclusion;
 }
 Console.WriteLine();
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Задача 38");
+int[] array3 = GetArray(10, 1, 99); //new int[5] {3, 7, 22, 2, 78};
+int[] minmax = SearchMinMax(array3);
+
+//Console.WriteLine($"[{String.Join(",", minmax)}]");
+Console.WriteLine($"[{String.Join(",", array3)}] -> {minmax[1] - minmax[0]}");
+
+int[] SearchMinMax(int[] array){
+    int[] minmax = new int[2] {array[1], array[1]};
+    for (int i = 0; i < array.Length; i++){
+        if (minmax[0] > array[i]){
+            minmax[0] = array[i];
+        }
+        else if (minmax[1] < array[i]){
+            minmax[1] = array[i];
+        }
+    }
+    return minmax;
+}
+Console.WriteLine();
+
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++){
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
