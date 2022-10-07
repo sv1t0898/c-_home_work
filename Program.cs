693a5378d93c3746d36f4f47f14067995f084f7e
dude <dude@dude.ru>
2022-10-07 21:40:52 +0300

    homework_lesson_6

--- Program.cs
+++ Program.cs
@@ -1,70 +1,56 @@
-﻿// Все массивы от 5-ти элементов
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
 
-// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+Console.WriteLine("Задача 41");
+int[] array = GenArray();
+int index = 0;
 
-Console.WriteLine("Задача 34");
-int[] array1 = GetArray(15, 100, 999);
-Console.WriteLine($"[{String.Join(",", array1)}] -> {ParytiCheck(array1)}");
-
-int ParytiCheck(int[] array){
-    int index = 0;
-    for (int i = 0; i < array.Length; i++){
-        if (array[i] % 2 == 0){
-            index++;
-        }
+for (int i = 0; i < array.Length; i++){
+    if (array[i] > 0){
+        index++;
     }
-    return index;
 }
+
+Console.WriteLine($"[{String.Join(",", array)}] -> {index}");
+
+int[] GenArray(){
+    Console.Write("Введите значене М: ");
+    int[] array = new int[int.Parse(Console.ReadLine()!)];
+    Console.WriteLine("Введите ряд чисел: ");
+    for (int i = 0; i < array.Length; i++){
+        array[i] = int.Parse(Console.ReadLine()!);  
+    }
+    return array; 
+}
+
 Console.WriteLine();
 
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
+Console.WriteLine("Задача 43");
+double[] bk1 = GetCoordinates(1);
+double[] bk2 = GetCoordinates(2);
+Console.Write($"b1 = {bk1[0]}, k1 = {bk1[1]}, b2 = {bk2[0]}, k2 = {bk2[1]} -> ");
+Console.WriteLine($"({String.Join("; ", FindCoordinates(bk1, bk2))})");
 
-Console.WriteLine("Задача 36");
-int[] array2 = GetArray(7, -11, 99); //new int[4] {-4, -6, 89, 6};
-Console.WriteLine($"[{String.Join(",", array2)}] -> {SumElements(array2)}");
-
-int SumElements(int[] array){
-    int conclusion = 0;
-    for (int i = 1; i < array2.Length; i += 2){
-        conclusion += array2[i]; 
-    }
-    return conclusion;
-}
-Console.WriteLine();
-
-
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-// [3 7 22 2 78] -> 76
-
-Console.WriteLine("Задача 38");
-int[] array3 = GetArray(10, 1, 99); //new int[5] {3, 7, 22, 2, 78};
-int[] minmax = SearchMinMax(array3);
-
-//Console.WriteLine($"[{String.Join(",", minmax)}]");
-Console.WriteLine($"[{String.Join(",", array3)}] -> {minmax[1] - minmax[0]}");
-
-int[] SearchMinMax(int[] array){
-    int[] minmax = new int[2] {array[1], array[1]};
+double[] GetCoordinates(int index_b_k){
+    double[] array = new double[2];
+    string xy = "bk";
     for (int i = 0; i < array.Length; i++){
-        if (minmax[0] > array[i]){
-            minmax[0] = array[i];
-        }
-        else if (minmax[1] < array[i]){
-            minmax[1] = array[i];
-        }
+        Console.WriteLine($"Задайте значение {xy[i]}{index_b_k}: ");
+        array[i] = double.Parse(Console.ReadLine()!);  
     }
-    return minmax;
+    return array; 
 }
-Console.WriteLine();
 
-int[] GetArray(int size, int minValue, int maxValue){
-    int[] array = new int[size];
-    for (int i = 0; i < array.Length; i++){
-        array[i] = new Random().Next(minValue, maxValue);
-    }
+double[] FindCoordinates(double[] xy1, double[] xy2){
+    double x = (xy1[0] - xy2[0])/((-xy1[1]) - (-xy2[1]));
+    double y = xy2[1] * x + xy2[0];
+    double[] array = new double[2] {x,y};
     return array;
 }
+
+Console.WriteLine();
